59f8cb62f29aecd9561081f64f2c81f7f422d66c
AMukherjee9 <AMukherjee9@slb.com>
2020-09-01 09:40:31 +0530

    first commit

---
+++ src/main/java/tech/getarrays/GetArraysEmployeeManagerApplication.java
@@ -0,0 +1,13 @@
+package tech.getarrays;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GetArraysEmployeeManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GetArraysEmployeeManagerApplication.class, args);
+	}
+
+}
